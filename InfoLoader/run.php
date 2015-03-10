eea4827aa0bde6110cb8486bb674c4a2ce3e83bf
bkralik <bkralik@hkfree.org>
2015-03-10 14:36:07 +0100

    Přechod na Composer a QueryPath místo phpQuery

--- InfoLoader/run.php
+++ InfoLoader/run.php
@@ -1,7 +1,7 @@
 <?php
 require_once('config.php');
-require_once('libs/dibi.php');
-require_once('libs/phpQuery.php');
+
+require_once('vendor/autoload.php');
 
 dibi::connect($DBOptions);
 
@@ -16,10 +16,19 @@ curl_setopt($ch, CURLOPT_COOKIEFILE, $tmpFile);
 curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 curl_exec($ch);
 
-for($l = 1; $l <= 100; $l++) {
+for($l = 1; $l <= 1; $l++) {
     curl_setopt($ch, CURLOPT_URL, "http://www.darkelf.cz/l.asp?id=".$l);
-    $output = iconv('CP1250','utf-8', curl_exec($ch));
-    echo($output);
+    //$output = iconv('CP1250','utf-8', curl_exec($ch));
+    $output = curl_exec($ch);
+    echo(iconv('CP1250','utf-8', $output));
+    echo("-------------\n");
+    $doc = qp($output, 'table');
+    $landName =  $doc->find('th:contains(Ciz)')->text();
+    echo($landName);
+    $td1 =  $doc->find('td:contains(vojska)');
+    $t = $td1->next()->text();
+    echo($t);
+    echo("-------------\n");
 }
 curl_close($ch);    
 
