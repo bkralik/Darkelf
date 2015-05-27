2e3ef8524f02ae5d6c11df4b4a0d2181d41f72e7
bkralik <bkralik@hkfree.org>
2015-05-27 21:04:51 +0200

    Small buildsystem fix

--- DeScriptsSource/build.sh
+++ DeScriptsSource/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+rm -rf ./output
+mkdir output
+mkdir output/scripts
 php descripts.php > output/index.html
 php updates.php > output/updates.html
 php scripts/db.php > output/scripts/db.js
 rm -rf ../descripts/*
+mkdir ../descripts
+cp assets/* ../descripts -r
 cp output/* ../descripts -r
